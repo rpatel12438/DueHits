2151106ee54922633341513d76974de33dfa5946
Roshan Patel <43523635-rpatel12438@users.noreply.replit.com>
2025-06-05 17:57:40 +0000

    Dropped unimportant columns from the DF

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
 # Load the CSV file
-df = pd.read_csv('topbatters.csv')  # Replace with the actual filename
+df = pd.read_csv('topbatters.csv') 
 
-# Preview the data
+# Show column names
+##print(df.columns.tolist())
+
+df.drop(columns = ['Age', 'Lg', 'WAR', 'G', 'R', '2B', '3B', 'HR', 'RBI', 'SB', 'CS', 'SO', 'OPS+', 'rOBA',
+                 'Rbat+', 'TB', 'GIDP', 'HBP', 'SH', 'SF', 'IBB', 'Pos', 'Awards', 'Player-additional'], inplace= True)
+# Add "inplace = True" if you don't want to reassign the DataFrame
+# No "inplace = True" you had to reassign the DataFrame to a new named dataframe
 print(df.head())
+
