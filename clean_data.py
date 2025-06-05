c148a0ff8916748986414b37d9acffade6ee74f8
Roshan Patel <rpatel12438@gmail.com>
2025-06-05 18:31:23 +0000

    Created separate file/function for cleaning

--- clean_data.py
+++ clean_data.py
@@ -1,14 +1,18 @@
 import pandas as pd
 
-# Load the CSV file
-df = pd.read_csv('topbatters.csv') 
+def clean_data():
+    # Load the CSV file
+    df = pd.read_csv('topbatters.csv')
 
-# Show column names
-##print(df.columns.tolist())
+    # Drop unwanted columns
+    df.drop(columns=[
+        'Age', 'Lg', 'WAR', 'G', 'R', '2B', '3B', 'HR', 'RBI', 'SB', 'CS', 'SO',
+        'OPS+', 'rOBA', 'Rbat+', 'TB', 'GIDP', 'HBP', 'SH', 'SF', 'IBB', 'Pos',
+        'Awards', 'Player-additional'
+    ], inplace=True)
 
-df.drop(columns = ['Age', 'Lg', 'WAR', 'G', 'R', '2B', '3B', 'HR', 'RBI', 'SB', 'CS', 'SO', 'OPS+', 'rOBA',
-                 'Rbat+', 'TB', 'GIDP', 'HBP', 'SH', 'SF', 'IBB', 'Pos', 'Awards', 'Player-additional'], inplace= True)
-# Add "inplace = True" if you don't want to reassign the DataFrame
-# No "inplace = True" you had to reassign the DataFrame to a new named dataframe
-print(df.head())
-df.to_csv('cleaned_batters.csv', index=False)
+    # Print first few rows
+    print(df.head())
+
+    # Save cleaned dataframe to new CSV
+    df.to_csv('cleaned_batters.csv', index=False)
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 import pandas as pd
+from clean_data import clean_data
 
-# Load the CSV file
-df = pd.read_csv('topbatters.csv') 
-
-# Show column names
-##print(df.columns.tolist())
-
-df.drop(columns = ['Age', 'Lg', 'WAR', 'G', 'R', '2B', '3B', 'HR', 'RBI', 'SB', 'CS', 'SO', 'OPS+', 'rOBA',
-                 'Rbat+', 'TB', 'GIDP', 'HBP', 'SH', 'SF', 'IBB', 'Pos', 'Awards', 'Player-additional'], inplace= True)
-# Add "inplace = True" if you don't want to reassign the DataFrame
-# No "inplace = True" you had to reassign the DataFrame to a new named dataframe
-print(df.head())
-df.to_csv('cleaned_batters.csv', index=False)
+clean_data()
